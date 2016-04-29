899308c639571f846800282ae9102f1d5c79812a
meteorshower <576466447@qq.com>
2016-04-29 11:44:15 +0800

    just beginning

--- ml/test_numpy.py
+++ ml/test_numpy.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from docx import Document
+from docx.table import  Table
 
 def linalg():
     a = np.array([[5,-1,2], [-2,6,9], [-7,5,-3]])
@@ -14,6 +15,9 @@ def linalg():
 
 if __name__ == "__main__":
     d = Document('20160509.docx')
+    for tab in d.tables:
+        t = tab.cell(0,0).text
+        pass
     for sec in d.sections:
         print sec.start_type
     pass
